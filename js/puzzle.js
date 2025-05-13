d82157544f7af8dcb3f53428e2f097f33f648b53
Daniel Virreira <virreira.daniel@gmail.com>
2025-05-12 23:30:48 -0400

    Añadir modularizacion a reinciar el puzzle.

--- js/puzzle.js
+++ js/puzzle.js
@@ -131,24 +131,3 @@ pieces.forEach(piece => {
     /* Evitar drag fantasma */
     piece.addEventListener('dragstart', e => e.preventDefault());
 });
-
-document.getElementById("reset").addEventListener('click', () => {
-    const containerRect = container.getBoundingClientRect();
-
-    pieces.forEach(piece => {
-        if (piece.classList.contains('in-board')) {
-            container.appendChild(piece);
-            piece.classList.remove('in-board');
-            piece.style.position = 'absolute';
-        }
-
-        const offsetX = Math.floor(Math.random() * (container.clientWidth - 80));
-        const offsetY = Math.floor(Math.random() * (container.clientHeight - 80));
-
-        piece.style.left = `${offsetX}px`;
-        piece.style.top = `${offsetY}px`;
-        piece.dataset.originalX = offsetX;
-        piece.dataset.originalY = offsetY;
-        piece.style.zIndex = '';
-    });
-});
