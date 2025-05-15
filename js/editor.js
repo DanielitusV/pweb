b2a683a85f1bd1a84f9c62ee419c5671749f257b
JAVI24-25 <201902218@est.umss.edu>
2025-05-15 18:15:37 -0400

    agregue una funcion para que me permita agregar la pregunta  y la dificutad

---
+++ js/editor.js
@@ -0,0 +1,5 @@
+function mostrarFormulario() {
+      const form = document.getElementById("formularioPregunta");
+      form.style.display = form.style.display === "none" ? "block" : "none";
+    }
+  
