e697e259c8fb2bd1946e6938711eb28da6a7ee71
Daniel Virreira <virreira.daniel@gmail.com>
2025-05-21 11:04:19 -0400

    Correción de bugs con el tablero y las piezas

--- js/puzzle-scripts/puzzle-image.js
+++ js/puzzle-scripts/puzzle-image.js
@@ -2,7 +2,8 @@ export function setupImageUpload(inputId, previewId, messageId) {
     const input = document.getElementById(inputId);
     const preview = document.getElementById(previewId);
     const message = document.getElementById(messageId);
-
+    const placeHolder = document.getElementById("uploadPlaceholder");
+    
     input.addEventListener('change', function (e) {
         const file = e.target.files[0];
 
@@ -11,12 +12,14 @@ export function setupImageUpload(inputId, previewId, messageId) {
             reader.onload = function (event) {
                 preview.src = event.target.result;
                 preview.style.display = 'block';
+                placeHolder.style.display = 'none';
                 message.textContent = '✅ Imagen subida correctamente';
                 message.style.color = 'green';
             };
             reader.readAsDataURL(file);
         } else {
             preview.style.display = 'none';
+            placeHolder.style.display = 'block';
             message.textContent = '❌ Error: El archivo no pudo subirse o no es valido';
             message.style.color = 'red';
         }
