97454d3f7446284b3e29dbab0721d536bb51c0fe
JAVI24-25 <201902218@est.umss.edu>
2025-06-10 03:29:47 -0400

    "nos creamos un archivo par ael admi para sus funciones necesarias"

---
+++ js/admin.js
@@ -0,0 +1,8 @@
+
+        function toggleSidebar() {
+            const sidebar = document.getElementById("sidebar");
+            const mainContent = document.getElementById("main-content");
+            sidebar.classList.toggle("open");
+            mainContent.classList.toggle("shifted");
+        }
+ 
